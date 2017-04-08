48d7427409848edc0d24e41079b0d5cb72c9c1d0
homulvas <ignas@ignas.co>
2017-04-08 18:25:49 +0300

    ipaddress, language, and software fields

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ var express = require('express')
 var app = express()
 
 app.get('/*', function (req, res) {
-  var result = { }
-  
+  var result = {}
+  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+  result.ipaddress = ip
+  result.language = req.headers['accept-language'].split(',')[0]
+  result.software = req.headers['user-agent'].match(/\(([^\)]+)\)/)[1]
+
   res.send(result)
 })
 
